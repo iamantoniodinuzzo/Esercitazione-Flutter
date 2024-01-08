41d4c4e3cd12a3197baf5cfc29a5b5e7dc98e6db
Antonio <tonydinu02@gmail.com>
2024-01-08 18:08:48 +0100

    Resa immutabile la classe Constants

--- movie_app/lib/util/constants.dart
+++ movie_app/lib/util/constants.dart
@@ -1,4 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+@immutable
 class Constants {
+  const Constants._();
   static const connectTimeout = 5000;
   static const receiveTimeout = 5000;
 }
