2fd34ceecb6bb33cc3a5cea8ae6f1756e818b749
Antonio <tonydinu02@gmail.com>
2024-01-08 14:52:04 +0100

    Migliorato l'header della schermata dettagli

--- movie_app/lib/res/components/media_poster.dart
+++ movie_app/lib/res/components/media_poster.dart
@@ -9,28 +9,34 @@ class MediaPoster extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Card(
-        child: Row(
-          children: [
-            //Media poster
-            SizedBox(
-              height: 150,
-              width: 100,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(5.0),
-                child: CachedNetworkImage(
-                  imageUrl: movie.completePosterPathW500,
-                  progressIndicatorBuilder: (context, url, progress) =>
-                      CircularProgressIndicator(
-                    value: progress.progress,
-                  ),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
+    return Card(
+      child: Row(
+        children: [
+          //Media poster
+          SizedBox(
+            height: 150,
+            width: 100,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5.0),
+              child: CachedNetworkImage(
+                imageUrl: movie.completePosterPathOriginal,
+                progressIndicatorBuilder: (context, url, progress) => Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(child: Text(movie.title)),
+                    const SizedBox(height: 5.0),
+                    Center(
+                      child: CircularProgressIndicator(
+                        value: progress.progress,
+                      ),
+                    ),
+                  ],
                 ),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
--- movie_app/lib/views/detail_screen.dart
+++ movie_app/lib/views/detail_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/data/remote/repository/movie_repository.dart';
 import 'package:movie_app/domain/model/movie/movie.dart';
 import 'package:movie_app/domain/model/movie/movie_details.dart';
+import 'package:movie_app/res/components/media_poster.dart';
 import 'package:provider/provider.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -25,30 +28,76 @@ class _DetailScreenState extends State<DetailScreen> {
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return [
             SliverAppBar(
-              expandedHeight: 200.0,
-              floating: true,
+              expandedHeight: 300.0,
+              floating: false,
               pinned: true,
+              snap: false,
               flexibleSpace: FlexibleSpaceBar(
-                title: Text(widget.selectedMedia.title),
-                background: Hero(
-                  tag: widget.selectedMedia.id,
-                  child: CachedNetworkImage(
-                    imageBuilder: (context, imageProvider) => Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: imageProvider,
-                          fit: BoxFit.cover,
+                collapseMode: CollapseMode.parallax,
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    // * Immagine di sfondo
+                    CachedNetworkImage(
+                      imageBuilder: (context, imageProvider) => Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: imageProvider,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.black.withOpacity(0.5),
+                                Colors.transparent,
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
                         ),
                       ),
+                      imageUrl:
+                          widget.selectedMedia.completeBackdropPathOriginal,
+                      progressIndicatorBuilder: (context, url, progress) =>
+                          Center(
+                        child: CircularProgressIndicator(
+                          value: progress.progress,
+                        ),
+                      ),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.image_not_supported),
                     ),
-                    imageUrl: widget.selectedMedia.completePosterPathW500,
-                    progressIndicatorBuilder: (context, url, progress) =>
-                        CircularProgressIndicator(
-                      value: progress.progress,
+                    // * Contenuto sovrapposto
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          // Poster
+                          Hero(
+                            tag: widget.selectedMedia.id,
+                            child: MediaPoster(movie: widget.selectedMedia),
+                          ),
+                          const SizedBox(width: 16.0),
+                          Expanded(
+                            child: Text(
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                              widget.selectedMedia.title,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
+                  ],
                 ),
               ),
             ),
@@ -65,7 +114,6 @@ class _DetailScreenState extends State<DetailScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const SizedBox(height: 16.0),
                       Text(
                         'Overview',
                         style: Theme.of(context).textTheme.titleLarge,
@@ -79,7 +127,11 @@ class _DetailScreenState extends State<DetailScreen> {
                       Wrap(
                         spacing: 8.0,
                         children: (movieDetails?.genres ?? [])
-                            .map((genre) => Chip(label: Text(genre.name)))
+                            .map(
+                              (genre) => Chip(
+                                label: Text(genre.name),
+                              ),
+                            )
                             .toList(),
                       ),
                       const SizedBox(height: 16.0),
