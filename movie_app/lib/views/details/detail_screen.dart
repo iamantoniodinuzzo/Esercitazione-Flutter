24b7cb4ef38c66a42f0d2be4f6653259e900a20e
Antonio <tonydinu02@gmail.com>
2024-01-09 12:46:45 +0100

    Migliorato il design app bar della schermata dettagli

--- movie_app/lib/views/details/detail_screen.dart
+++ movie_app/lib/views/details/detail_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:movie_app/data/remote/repository/movie_repository.dart';
 import 'package:movie_app/domain/model/movie/movie.dart';
 import 'package:movie_app/domain/model/movie/movie_details.dart';
@@ -52,37 +53,40 @@ class _DetailScreenState extends State<DetailScreen> {
                     fit: StackFit.expand,
                     children: [
                       // * Immagine di sfondo
-                      CachedNetworkImage(
-                        imageBuilder: (context, imageProvider) => Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: imageProvider,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          child: Container(
+                      Positioned.fill(
+                        bottom: 100,
+                        child: CachedNetworkImage(
+                          imageBuilder: (context, imageProvider) => Container(
                             decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  Colors.black.withOpacity(0.5),
-                                  Colors.transparent,
-                                ],
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.topCenter,
+                              image: DecorationImage(
+                                image: imageProvider,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Colors.white.withOpacity(0.6),
+                                    Colors.transparent,
+                                  ],
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        imageUrl:
-                            widget.selectedMedia.completeBackdropPathOriginal,
-                        progressIndicatorBuilder: (context, url, progress) =>
-                            Center(
-                          child: CircularProgressIndicator(
-                            value: progress.progress,
+                          imageUrl:
+                              widget.selectedMedia.completeBackdropPathOriginal,
+                          progressIndicatorBuilder: (context, url, progress) =>
+                              Center(
+                            child: CircularProgressIndicator(
+                              value: progress.progress,
+                            ),
                           ),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.image_not_supported),
                         ),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.image_not_supported),
                       ),
                       // * Contenuto sovrapposto
                       Padding(
@@ -99,11 +103,11 @@ class _DetailScreenState extends State<DetailScreen> {
                             //* Titolo
                             Expanded(
                               child: Text(
-                                maxLines: 3,
+                                maxLines: 2,
                                 overflow: TextOverflow.ellipsis,
                                 widget.selectedMedia.title,
                                 style: const TextStyle(
-                                  color: Colors.white,
+                                  color: Colors.black,
                                   fontSize: 30.0,
                                   fontWeight: FontWeight.bold,
                                 ),
