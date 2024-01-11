e0195247d1d52951e359c482008595ffd5f961a6
Antonio <tonydinu02@gmail.com>
2024-01-11 10:46:00 +0100

    Pulito il codice in media poster

--- movie_app/lib/res/components/media_poster.dart
+++ movie_app/lib/res/components/media_poster.dart
@@ -17,51 +17,53 @@ class MediaPoster extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        children: [
-          SizedBox(
-            height: 150,
-            width: 100,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5.0),
-              child: Stack(
-                alignment: AlignmentDirectional.topEnd,
-                children: [
-                  CachedNetworkImage(
-                    imageUrl: movie.completePosterPathOriginal,
-                    progressIndicatorBuilder: (context, url, progress) =>
-                        Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                        child: Text(
-                          movie.title,
-                          style: const TextStyle(fontSize: 10),
-                        ),
-                      ),
-                    ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
+      child: SizedBox(
+        height: 150,
+        width: 100,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(5.0),
+          child: Stack(
+            alignment: AlignmentDirectional.topEnd,
+            children: [
+              _buildCachedNetworkImage(),
+              _buildVoteAverageChip(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  CachedNetworkImage _buildCachedNetworkImage() {
+    return CachedNetworkImage(
+              imageUrl: movie.completePosterPathOriginal,
+              progressIndicatorBuilder: (context, url, progress) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    movie.title,
+                    style: const TextStyle(fontSize: 10),
                   ),
-                  Visibility(
-                    visible: isVoteAverageVisible,
-                    child: Opacity(
-                      opacity: 0.5,
-                      child: Chip(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0), // Imposta il padding per allungare il chip
-                        backgroundColor: MovieAppColors.primary,
-                        label: Text(
-                          style: MovieAppTextStyle.secondaryPBold
-                              .copyWith(fontSize: 12),
-                          movie.voteAverage.ceilToDouble().toString(),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          )
-        ],
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            );
+  }
+
+  Widget _buildVoteAverageChip() {
+    return Visibility(
+      visible: isVoteAverageVisible,
+      child: Opacity(
+        opacity: 0.5,
+        child: Chip(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+          // Imposta il padding per allungare il chip
+          backgroundColor: MovieAppColors.primary,
+          label: Text(
+            style: MovieAppTextStyle.secondaryPBold.copyWith(fontSize: 12),
+            movie.voteAverage.ceilToDouble().toString(),
+          ),
+        ),
       ),
     );
   }
